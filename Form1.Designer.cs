111feb3d1dcd29d666f45b58c51379b03b65df08
Andrew <andrewmcdan@gmail.com>
2024-03-02 19:20:34 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace CleverDecksLauncher
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            notifyIcon1 = new NotifyIcon(components);
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            textBox1 = new TextBox();
+            autoScrollCheckBox = new CheckBox();
+            SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            notifyIcon1.ContextMenuStrip = contextMenuStrip1;
+            notifyIcon1.Icon = (Icon)resources.GetObject("notifyIcon1.Icon");
+            notifyIcon1.Text = "CleverDecks";
+            notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(61, 4);
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Cursor = Cursors.IBeam;
+            textBox1.HideSelection = false;
+            textBox1.Location = new Point(1, 0);
+            textBox1.MaxLength = 131071;
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.ScrollBars = ScrollBars.Vertical;
+            textBox1.Size = new Size(623, 314);
+            textBox1.TabIndex = 1;
+            // 
+            // autoScrollCheckBox
+            // 
+            autoScrollCheckBox.AutoSize = true;
+            autoScrollCheckBox.Checked = true;
+            autoScrollCheckBox.CheckState = CheckState.Checked;
+            autoScrollCheckBox.Location = new Point(520, -1);
+            autoScrollCheckBox.Name = "autoScrollCheckBox";
+            autoScrollCheckBox.Size = new Size(84, 19);
+            autoScrollCheckBox.TabIndex = 2;
+            autoScrollCheckBox.Text = "Auto Scroll";
+            autoScrollCheckBox.UseVisualStyleBackColor = true;
+            autoScrollCheckBox.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(624, 314);
+            Controls.Add(autoScrollCheckBox);
+            Controls.Add(textBox1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            ShowInTaskbar = false;
+            Text = "Form1";
+            WindowState = FormWindowState.Minimized;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private NotifyIcon notifyIcon1;
+        private ContextMenuStrip contextMenuStrip1;
+        private TextBox textBox1;
+        private CheckBox autoScrollCheckBox;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,209 @@
+using System.Diagnostics;
+using System.Windows.Forms;
+using System;
+using System.Windows.Forms;
+
+namespace CleverDecksLauncher
+{
+    public partial class Form1 : Form
+    {
+        private Process nodeProcess;
+        private bool isCleverDecksRunning = false;
+        private string[] cl_args;
+        public Form1(string[] args)
+        {
+            cl_args = args;
+            InitializeComponent();
+            InitializeSystemTrayOptions();
+            InitializeReadOnlyTextArea();
+            this.FormClosing += MainForm_FormClosing;
+            notifyIcon1.DoubleClick += notifyIcon1_DoubleClick;
+            this.Load += MainForm_Load;
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                this.WindowState = FormWindowState.Minimized;
+                this.ShowInTaskbar = false;
+            }
+        }
+
+        private void InitializeSystemTrayOptions()
+        {
+            var showOutputMenuItem = new ToolStripMenuItem("Show Log Window");
+            var launchBrowserMenuItem = new ToolStripMenuItem("Launch in Browser");
+            var quitMenuItem = new ToolStripMenuItem("Quit");
+
+            showOutputMenuItem.Click += ShowOutputMenuItem_Click;
+            launchBrowserMenuItem.Click += LaunchBrowserMenuItem_Click;
+            quitMenuItem.Click += QuitMenuItem_Click;
+
+            contextMenuStrip1.Items.Add(showOutputMenuItem);
+            contextMenuStrip1.Items.Add(launchBrowserMenuItem);
+            contextMenuStrip1.Items.Add(quitMenuItem);
+
+            notifyIcon1.ContextMenuStrip = contextMenuStrip1;
+            notifyIcon1.Visible = true;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+            LaunchNodeApp();
+        }
+
+        private void LaunchNodeApp()
+        {
+            var startInfo = new ProcessStartInfo
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+            };
+
+            if (cl_args.Length == 0)
+            {
+                startInfo.FileName = "CleverDecks.exe";
+            }
+            else if (cl_args.Length == 1 && cl_args[0].EndsWith(".exe"))
+            {
+                startInfo.FileName = cl_args[0];
+            }
+            else if (cl_args.Length == 1 && cl_args[0].EndsWith(".js"))
+            {
+                startInfo.FileName = "node";
+                startInfo.Arguments = cl_args[0];
+                int lastSlash = cl_args[0].LastIndexOf('/');
+                if (lastSlash == -1)
+                {
+                    lastSlash = cl_args[0].LastIndexOf('\\');
+                }
+                startInfo.WorkingDirectory = cl_args[0].Substring(0, lastSlash);
+            }
+            var startsInfo = new ProcessStartInfo
+            {
+                FileName = "node",
+                Arguments = "index.js",
+                WorkingDirectory = "O:/Projects/CleverDecks",
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+            };
+            nodeProcess = Process.Start(startInfo);
+            nodeProcess.OutputDataReceived += NodeProcess_OutputDataReceived;
+            nodeProcess.BeginOutputReadLine();
+            isCleverDecksRunning = true;
+
+        }
+
+        private void NodeProcess_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(e.Data))
+            {
+                // Invoke the UI thread to update the TextBox
+                this.Invoke(new Action(() =>
+                {
+                    textBox1.AppendText(e.Data + Environment.NewLine);
+                    if(autoScrollCheckBox.Checked)
+                    {
+                        textBox1.SelectionStart = textBox1.Text.Length;
+                        textBox1.ScrollToCaret();
+                    }
+                }));
+            }
+        }
+
+
+        private void ShowOutputMenuItem_Click(object sender, EventArgs e)
+        {
+            // Implement showing the output of the Node.js app
+            if (!isCleverDecksRunning)
+            {
+                ShowErrorNotification("CleverDecks is not running.");
+                textBox1.Text = "CleverDecks is not running. This is unusual and should be fixed by restarting the launcher. If that doesn't fix the problem, try re-installing the application.";
+            }
+            if (this.ShowInTaskbar)
+            {
+                this.ShowInTaskbar = false;
+                this.WindowState = FormWindowState.Minimized;
+            }
+            else
+            {
+                this.ShowInTaskbar = true;
+                this.WindowState = FormWindowState.Normal;
+                if (autoScrollCheckBox.Checked)
+                {
+                    textBox1.SelectionStart = textBox1.Text.Length;
+                    textBox1.ScrollToCaret();
+                }
+                this.Activate();
+            }
+
+        }
+
+        private void LaunchBrowserMenuItem_Click(object sender, EventArgs e)
+        {
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = "http://localhost:3000",
+                UseShellExecute = true
+            };
+            try
+            {
+                Process.Start(startInfo);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void QuitMenuItem_Click(object sender, EventArgs e)
+        {
+            nodeProcess?.Kill();
+            Application.Exit();
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            if (!isCleverDecksRunning)
+            {
+                ShowErrorNotification("CleverDecks is not running.");
+                textBox1.Text = "CleverDecks is not running. This is unusual and should be fixed by restarting the launcher. If that doesn't fix the problem, try re-installing the application.";
+            }
+            this.ShowInTaskbar = true;
+            this.WindowState = FormWindowState.Normal;
+            this.Activate();
+        }
+
+        private void ShowErrorNotification(string errorMessage)
+        {
+            // Parameters are: duration in milliseconds, title, message, and icon
+            notifyIcon1.ShowBalloonTip(5000, "Error Occurred", errorMessage, ToolTipIcon.Error);
+        }
+
+        private void InitializeReadOnlyTextArea()
+        {
+            //TextBox readOnlyTextArea = new TextBox();
+            textBox1.Multiline = true; // Enable multiline
+            textBox1.ReadOnly = true;  // Make it read-only
+            textBox1.ScrollBars = ScrollBars.Vertical; // Add a vertical scrollbar
+            textBox1.Dock = DockStyle.Fill; // Optional: Fill the parent container
+
+            // Set the initial text (if any)
+            textBox1.Text = "";
+
+            // Add the TextBox to the form (or a specific container on the form)
+            this.Controls.Add(textBox1);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
